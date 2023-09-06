4d64c1bf293f4526dfaf984d5db8d3b1b37b82c7
EC2 Default User <ec2-user@ip-10-0-139-23.ap-northeast-1.compute.internal>
2023-09-06 03:58:41 +0000

    mod App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { NewHomes, MarketingFooter } from './ui-components'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+return (
+<div className="App">
+<NewHomes />
+<MarketingFooter />
+</div>
+);
 }
 
 export default App;
